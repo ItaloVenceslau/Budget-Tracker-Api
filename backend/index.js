6aa8eb8e1a4a29e9d7a8a83af980ce11c6c1bd7c
unknown <ramalhoitalo26@gmail.com>
2026-05-13 13:34:52 -0300

    first commit

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 // index.js
 require('dotenv').config();
-const app = require('./src/app');
-const connectDB = require('./src/config/database');
+const app = require('./app');
+const connectDB = require('./config/database');
 
 const PORT = process.env.PORT || 5000;
 
