8b54fc3e546caa96add95544c4fb0b55ad050ed7
unknown <ramalhoitalo26@gmail.com>
2026-05-13 18:06:40 -0300

    solving problems

--- frontend/src/components/Auth/ProtectedRoute.jsx
+++ frontend/src/components/Auth/ProtectedRoute.jsx
@@ -1,7 +1,7 @@
 import React, { useContext } from 'react';
 import { Navigate } from 'react-router-dom';
 import { AuthContext } from '../../context/AuthContext';
-import { LoadingSpinner } from '../Common/LoadingSpinner';
+import { LoadingSpinner } from '../Common/loadingSpinner';
 
 export const ProtectedRoute = ({ children }) => {
   const { user, loading } = useContext(AuthContext);
