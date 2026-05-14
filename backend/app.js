2d7f2a5fdd93f46dc3b31e7dba605b54fa0ebdb9
unknown <ramalhoitalo26@gmail.com>
2026-05-14 18:33:12 -0300

    solving backend problems

--- backend/app.js
+++ backend/app.js
@@ -11,8 +11,17 @@ dotenv.config();
 
 const app = express();
 
-// Middlewares globais
 app.use(cors());
+app.use(cors({
+  origin: [
+    'https://budget-tracker-frontend.vercel.app',
+    'http://localhost:5173',
+    'http://localhost:3000'
+  ],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
